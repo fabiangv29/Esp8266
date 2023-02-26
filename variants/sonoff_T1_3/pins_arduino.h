8db4bd12c5085767b8c0380c8863f79f3d224ac3
fabiann_gv <fabiann_gv@hotmail.com>
2023-02-25 20:08:50 -0600

    Add sonoff t1 3 board
    Signed-off-by: fabiann_gv <fabiann_gv@hotmail.com>

---
+++ variants/sonoff_T1_3/pins_arduino.h
@@ -0,0 +1,30 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#define PIN_WIRE_SDA (4)
+#define PIN_WIRE_SCL (5)
+
+static const uint8_t SDA = PIN_WIRE_SDA;
+static const uint8_t SCL = PIN_WIRE_SCL;
+
+//Buttons pins 
+#define BUTTON_ACTIVE_LOW 1
+#define PULL_UP_ACTIVE 1
+#define BUTTON_0_PIN 0
+#define BUTTON_1_PIN 9
+#define BUTTON_2_PIN 10
+
+//Outputs pins
+#define INITIAL_OUTPUT_STATE LOW 
+#define OUTPUT_0_PIN 12
+#define OUTPUT_1_PIN 5
+#define OUTPUT_2_PIN 4
+
+//Status LED
+#define STATUS_LED 13
+#define NO_CONNECTION_STATUS_LED HIGH
+#define CONNECTION_STATUS_LED LOW
+
+#include "../generic/common.h"
+
+#endif /* Pins_Arduino_h */
